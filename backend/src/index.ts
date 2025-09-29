f46729d8f91b0a4310f68ac6dcca01e037fef792
Kris <krishnakumars0101@gmail.com>
2025-09-30 00:56:28 +0530

    data checkout using get method successful

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import todos from "./routes/todos";
 
 const app = express();
 dotenv.config();
@@ -10,11 +11,14 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/todos",todos);
+
 app.get("/",(req,res)=>{
     res.status(200).json({
         message:'hello from backend'
     })
 })
+
 
 app.listen(PORT ,() =>{
     console.log(`server is listening to port http://localhost:${PORT}/`);
--- backend/src/routes/todos.ts
+++ backend/src/routes/todos.ts
@@ -5,4 +5,9 @@ import type { Todo } from "../types.js";
 const router = Router();
 
 
+router.get("/",async (_,res)=>{
+    const result = await pool.query<Todo>("SELECT * FROM todos ORDER BY id DESC");
+    res.json(result.rows);
+})
 
+export default  router;
